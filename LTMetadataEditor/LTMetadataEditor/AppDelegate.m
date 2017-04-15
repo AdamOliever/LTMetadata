11ac4f0d3a8f93bb22e9cf7aee7ad590a7b71edc
Adam <976660802@qq.com>
2017-04-15 22:20:30 +0800

    RootviewController modify

--- LTMetadataEditor/LTMetadataEditor/AppDelegate.m
+++ LTMetadataEditor/LTMetadataEditor/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "Photo/LTPhotoController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [[LTPhotoController alloc]init];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
